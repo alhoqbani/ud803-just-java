3eeb2d091b6233ec2110ff2e60452d83e60142b8
Hamoud Alhoqbani <h.alhoqbani@gmail.com>
2018-08-07 14:22:31 -0700

    Add global quantity variable to hold the quantity value

--- app/src/main/java/com/alhoqbani/justjava/MainActivity.java
+++ app/src/main/java/com/alhoqbani/justjava/MainActivity.java
@@ -9,6 +9,8 @@ import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
 
+    int quantity = 2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,9 +21,8 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        int numberOfCoffees = 2;
-        display(numberOfCoffees);
-        displayPrice(numberOfCoffees * 2);
+        display(quantity);
+        displayPrice(quantity * 2);
     }
 
     /**
@@ -46,9 +47,8 @@ public class MainActivity extends AppCompatActivity {
      * @param view The Button view.
      */
     public void increment(View view) {
-        int numberOfCoffees = 3;
-        display(numberOfCoffees);
-        displayPrice(numberOfCoffees * 2);
+        quantity = quantity + 1;
+        display(quantity);
     }
 
     /**
@@ -57,8 +57,6 @@ public class MainActivity extends AppCompatActivity {
      * @param view The Button view.
      */
     public void decrement(View view) {
-        int numberOfCoffees = 1;
-        display(numberOfCoffees);
-        displayPrice(numberOfCoffees * 2);
+        quantity = quantity - 1;
     }
 }
