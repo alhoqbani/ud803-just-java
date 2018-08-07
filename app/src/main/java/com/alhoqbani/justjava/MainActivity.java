2ae776ef9f79af3165a6b33a5274a8833c22ebc6
Hamoud Alhoqbani <h.alhoqbani@gmail.com>
2018-08-07 15:35:42 -0700

    Experiment with add TextView from inside Java class in (onCreate)

--- app/src/main/java/com/alhoqbani/justjava/MainActivity.java
+++ app/src/main/java/com/alhoqbani/justjava/MainActivity.java
@@ -1,5 +1,6 @@
 package com.alhoqbani.justjava;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -14,7 +15,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        TextView textView = new TextView(this);
+        textView.setText("lalalalalalalalalalalalalalalalalalalalalalalalalala");
+        textView.setTextSize(56);
+        textView.setTextColor(Color.GREEN);
+        textView.setMaxLines(2);
+
+        setContentView(textView);
     }
 
     /**
